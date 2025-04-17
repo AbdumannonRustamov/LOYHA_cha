f7df93e0a96bc8b255bc293f4d2798a3dd17c1bc
m <m>
2025-04-17 14:39:34 +0500

    us

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHr_g6SlKfPRcYbrbYuXsT-Izdgku0ahQ",
+  authDomain: "newloyiha.firebaseapp.com",
+  projectId: "newloyiha",
+  storageBucket: "newloyiha.firebasestorage.app",
+  messagingSenderId: "404775119715",
+  appId: "1:404775119715:web:c40f45fb6b4c88e5678156"
+};
+
+export const app = initializeApp(firebaseConfig);
