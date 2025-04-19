515be994a79b19957a85eafc629c765f81de8c6e
m <m>
2025-04-19 21:36:25 +0500

    fe

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom"
-import Sidebar from "../components/Sidebar"
 
 function MainLayout() {
   return (
     <>
-      <Sidebar/>
     <main>
         <Outlet/>
     </main>
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
-import './overview.css'
+import './overview.css';
+
 function Overview() {
   const navigate = useNavigate();
 
@@ -15,8 +16,23 @@ function Overview() {
   };
 
   return (
-    <div>
-      <button className="btn btn-success" onClick={handleLogout}>Log Out</button>
+    <div className="container">
+      <aside className="sidebar">
+        <h2 className="logo">finance</h2>
+        <nav className="nav-links">
+          <a href="#" >Overview</a>
+          <a href="/transactions">Transactions</a>
+          <a href="/budgets">Budgets</a>
+          <a href="/posts">Pots</a>
+          <a href="/recurringBills">Recurring Bills</a>
+        </nav>
+        <div className="minimize">Minimize Menu</div>
+      </aside>
+
+      <main className="main-content">
+        <h1>Overview</h1>
+        <button className="btn btn-success" onClick={handleLogout}>Log Out</button>
+      </main>
     </div>
   );
 }
