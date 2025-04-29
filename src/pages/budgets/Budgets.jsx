6b8a3b1c574a7390c499275047898cace2b517cc
m <m>
2025-04-29 09:40:59 +0500

    dv

--- src/pages/budgets/Budgets.jsx
+++ src/pages/budgets/Budgets.jsx
@@ -4,9 +4,12 @@
   export default function Budgets() {
     return (
       <div className="budgets-container">
-        <div className='header'>
+        <div className='header'>  
+        <div className='header-2'>
         <h1>Budgets</h1>
-          <div>
+        <button className="add-budget-btn">+ Add New Budget</button>
+        </div>
+          <div className='chart'>
             <ApexChart/>
             </div>
             </div>
@@ -33,7 +36,19 @@
             </ul>
           </div>
         </section>
-
+        
+        <section className="budget-category">
+          <h3>Personal Care</h3>
+          <div className="budget-bar"></div>
+          <div className="latest-spending">
+            <h4>Latest Spending</h4>
+            <ul>
+              <li><span>Barber</span><span>$30</span></li>
+              <li><span>Salon</span><span>$30</span></li>
+            </ul>
+          </div>
+        </section>
+          
         <section className="budget-category">
           <h3>Dining Out</h3>
           <div className="budget-bar"></div>
@@ -46,17 +61,7 @@
           </div>
         </section>
 
-        <section className="budget-category">
-          <h3>Personal Care</h3>
-          <div className="budget-bar"></div>
-          <div className="latest-spending">
-            <h4>Latest Spending</h4>
-            <ul>
-              <li><span>Barber</span><span>$30</span></li>
-              <li><span>Salon</span><span>$30</span></li>
-            </ul>
-          </div>
-        </section>
+       
       </div>
     );
   }
--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,10 +1,161 @@
-
+import "./Post.css";
 function Posts() {
   return (
     <div>
-      posts
+      <div className="father-post">
+        <h1>Budgets</h1>
+        <button className="add-budget-btn">+ Add New Budget</button>
+      </div>
+      <div className="savings-container">
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span> Savings</span>
+            <span className="savings-amount">$159.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span> Concert Ticket</span>
+            <span className="savings-amount">$110.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span>Gift</span>
+            <span className="savings-amount">$40.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span>New Laptop</span>
+            <span className="savings-amount">$10.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span>Holiday</span>
+            <span className="savings-amount">$531.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span> Savings</span>
+            <span className="savings-amount">$159.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+
+        <div className="savings-card">
+          <div className="savings-card-header">
+            <span> Savings</span>
+            <span className="savings-amount">$159.00</span>
+          </div>
+          <div className="progress-bar">
+            <div
+              className="progress-fill color-green"
+              style={{ width: "7.95%" }}
+            ></div>
+          </div>
+          <div className="progress-info">
+            <span>7.95%</span>
+            <span>Target of $2,000</span>
+          </div>
+          <div className="savings-actions">
+            <button>+ Add Money</button>
+            <button>Withdraw</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
